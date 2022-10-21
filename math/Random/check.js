eeb32c0a2bb0b089ba31de9d3b72b8cb7b05afe3
Sianglife <88283637+Sianglife@users.noreply.github.com>
2022-10-21 05:21:39 +0000

    update

--- math/Random/check.js
+++ math/Random/check.js
@@ -2,5 +2,6 @@ function ongo() {
     var low = $('#low').val();
     var high = $('#high').val();
     var out = (Math.floor(Math.random() * high) + low)
+    console.log(low,high,out)
     $('#output').html(out);
 }

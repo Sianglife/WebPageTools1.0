079fe0bbdcc2b95b5e1befb012c461b1bb349e5f
Sianglife <88283637+Sianglife@users.noreply.github.com>
2022-11-10 23:39:39 +0800

    delete all resource

--- math/Random/check.js
+++
@@ -1,8 +0,0 @@
-function ongo() {
-    // var low = $('#low').val();
-    var low = 1;
-    var high = $('#high').val();
-    var out = String(Math.floor(Math.random() * high) + parseInt(low))
-    console.log(low,high,out)
-    $('#output').html(out);
-}
